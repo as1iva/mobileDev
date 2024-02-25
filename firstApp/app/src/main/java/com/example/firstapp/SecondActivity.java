5a1949caea1c5bc9d8db91a19e10b688e845610b
Tim <essentialforlife17@gmail.com>
2024-02-25 19:07:08 +0300

    first commit

---
+++ firstApp/app/src/main/java/com/example/firstapp/SecondActivity.java
@@ -0,0 +1,36 @@
+package com.example.firstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        Intent intent = getIntent();
+        String receivedText = intent.getStringExtra("textFromMain");
+
+        TextView textView = findViewById(R.id.textView);
+        String message = getString(R.string.hamsterNameMessage, receivedText);
+
+        textView.setText(message);
+
+        Button button = findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SecondActivity.this, MainActivity.class);
+
+                startActivity(intent);
+            }
+        });
+    }
+}
